bc9041cf3133b4b0c5a68b515f2f625fd34a6e7c
tanmui1992 <tanmui1992@gmail.com>
2023-01-27 14:48:19 +0700

    feat: Test and code checkbox functionality

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 
 function App() {
   const [buttonColor, setButtonColor] = useState("red");
+  const [disabled, setDisabled] = useState(false);
   const newButtonColor = buttonColor === "red" ? "blue" : "red";
   return (
     <div className="App">
@@ -11,9 +12,16 @@ function App() {
         onClick={() => {
           setButtonColor(newButtonColor);
         }}
+        disabled={disabled}
       >
         Change to {newButtonColor}
       </button>
+      <input
+        type="checkbox"
+        id="disable-button-checkbox"
+        defaultChecked={disabled}
+        onChange={(e) => setDisabled(e.target.checked)}
+      />
     </div>
   );
 }
