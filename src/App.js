c6f5e586ac519e7dac9bba7fe9ca922a726fd184
tanmui1992 <tanmui1992@gmail.com>
2023-01-27 18:13:43 +0700

    Feat: setup ESLint

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 export function replaceCamelWithSpaces(colorName) {
   return colorName.replace(/\B([A-Z])\B/g, " $1");
