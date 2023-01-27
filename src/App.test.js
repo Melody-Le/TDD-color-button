40ecc238f023033bc3713bc7c9b19baba6374ad5
tanmui1992 <tanmui1992@gmail.com>
2023-01-27 13:36:18 +0700

    Feat: test when button clicked

--- src/App.test.js
+++ src/App.test.js
@@ -1,18 +1,26 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import { logRoles } from "@testing-library/dom";
 import App from "./App";
 
-test("button has correct initial color", () => {
+test("button has correct initial color and update when click", () => {
   // render(<App />);
   // const linkElement = screen.getByRole("link", { name: "learn react" });
   // expect(linkElement).toBeInTheDocument();
   // // throw new Error("test failed");
-  const { container } = render(<App />);
-  logRoles(container);
+  // const { container } = render(<App />);
+  // logRoles(container);
+  render(<App />);
   // find an element with a role of button and text of "Change to blue"
   const colorButton = screen.getByRole("button", { name: "Change to blue" });
   // expect the background color to be red
   expect(colorButton).toHaveStyle({ backgroundColor: "red" });
+  // clicked button
+  fireEvent.click(colorButton);
+  // expect the background color to be blue
+  expect(colorButton).toHaveStyle({ backgroundColor: "blue" });
+  expect(colorButton).toHaveTextContent("Change to red");
 });
-test("button has correct initial text", () => {});
-test("button turns blue when clicked", () => {});
+// test("button turns blue when clicked", () => {
+//   render(<App />);
+//   const colorButton = screen.getByRole("button", { name: "Change to blue" });
+// });
